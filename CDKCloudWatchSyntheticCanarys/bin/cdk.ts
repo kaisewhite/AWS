42d31edaf654d7cf901281e90c539bad318321df
kaisewhite <kaisewhite@kais-app.com>
2020-12-12 16:03:53 -0500

    removed cdkout

--- CDKCloudWatchSyntheticCanarys/bin/cdk.ts
+++ CDKCloudWatchSyntheticCanarys/bin/cdk.ts
@@ -3,14 +3,9 @@ import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { CdkStack } from "../lib/cdk-stack";
 
-import { env } from "../env";
-
-let nonprod = { accountID: "699678132176", environment: "nonprod" };
-let sandbox4 = { accountID: "407747792847", environment: "sandbox4" };
-
 const app = new cdk.App();
 new CdkStack(app, "CdkStack", {
   env: {
-    account: "699678132176",
+    account: "*******",
   },
 });
